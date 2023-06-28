8a60ab739c5cf76e03771af14df25887b4829494
shanmukhakumar48 <135953207+shanmukhakumar48@users.noreply.github.com>
2023-06-28 20:48:48 +0530

    Add files via upload

---
+++ AbsoluteNumb.java
@@ -0,0 +1,15 @@
+
+public class AbsoluteNumb {  //positive nums are absolute numbers 
+	                                // 
+
+	public static void main(String[] args) {
+
+		double i=-78.799; 
+		int   j=-4;                            //Converting negative nums to positive nums using math.abs
+		long l=-88888888L;
+		System.out.println(Math.abs(i));
+		System.out.println(Math.abs(j));
+		System.out.println(Math.abs(l));
+	}
+
+}
